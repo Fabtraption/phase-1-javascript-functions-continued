e0d037f54178ac7bf3b32e5bca384aa3306787ac
Chris Bonet <christopher.bonet@gmail.com>
2022-11-22 16:25:58 -0500

    commit tree d6e327a45137dd33792e0139febec0a05cd5fd00

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun();
+saturdayFun(activity = "bathe my dog");
+
+function mondayWork(work = "go to the office" ) {
+    return `This Monday, I will ${work}.`
+}
+
+mondayWork;
+mondayWork(work = "work from home");
+
+function wrapAdjective() {
+
+};
+
